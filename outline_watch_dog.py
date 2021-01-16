259d3f936a94758ff07bfbd0627d7972cda1cc68
mzz <m@mzz.pub>
2021-01-17 01:11:51 +0800

    Revert "📂 o+x for directories"

--- outline_watch_dog.py
+++ outline_watch_dog.py
@@ -2,37 +2,36 @@ import os
 import pyinotify
 
 
-def change_permissions(path):
+def add_read_privilege(path):
     result = os.stat(path)
     mode = result.st_mode
-    if mode & os.stat.S_IROTH != os.stat.S_IROTH:
-        os.chmod(path, mode | os.stat.S_IROTH)
-    if os.path.isdir(path) and mode & os.stat.S_IXOTH != os.stat.S_IXOTH:
-        os.chmod(path, mode | os.stat.S_IXOTH)
+    rr = 0o0004
+    if mode & rr != rr:
+        os.chmod(path, mode | rr)
 
 
-def change_permissions_recursive(path):
-    change_permissions(path)
+def add_read_privilege_recursive(path):
+    add_read_privilege(path)
     for root, dirs, files in os.walk(path, True):
         for name in files + dirs:
-            change_permissions(os.path.join(root, name))
+            add_read_privilege(os.path.join(root, name))
 
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_ATTRIB(self, event):
-        change_permissions(event.pathname)
+        add_read_privilege(event.pathname)
 
     def process_IN_CREATE(self, event):
-        change_permissions(event.pathname)
+        add_read_privilege(event.pathname)
 
 
 def main():
     path = '/opt/outline'
-    change_permissions_recursive(path)
+    add_read_privilege_recursive(path)
 
     wm = pyinotify.WatchManager()
     mask = pyinotify.IN_ATTRIB | pyinotify.IN_CREATE
-    wm.add_watch(path, mask, rec=True)
+    wdd = wm.add_watch(path, mask, rec=True)
     handler = EventHandler()
     notifier = pyinotify.Notifier(wm, handler)
     notifier.loop()
